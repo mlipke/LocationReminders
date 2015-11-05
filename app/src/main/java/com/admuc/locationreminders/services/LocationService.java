5bfa0f329d91141b1428a27d7f892f0e629c0ea0
Matthes Lipke <matthes.lipke@gmail.com>
2015-11-05 14:09:38 +0100

    Added LocationService with Messenger ipc

---
+++ app/src/main/java/com/admuc/locationreminders/services/LocationService.java
@@ -0,0 +1,40 @@
+package com.admuc.locationreminders.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+
+public class LocationService extends Service {
+
+    static final int MSG_GET_POIS = 1;
+
+    final Messenger messenger = new Messenger(new MessageHandler());
+
+    public LocationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return messenger.getBinder();
+    }
+    
+    static class MessageHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case MSG_GET_POIS:
+                    Log.d("Location Service", "Getting data!");
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+
+}
