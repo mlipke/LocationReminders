d02cef55733ca6f636741ce3803d005170544c81
4gray <serega05@gmail.com>
2015-11-13 08:43:42 +0100

    marker can be settled now in SelectLocationActivity

--- app/src/main/java/com/admuc/locationreminders/activities/SelectLocationActivity.java
+++ app/src/main/java/com/admuc/locationreminders/activities/SelectLocationActivity.java
@@ -1,19 +1,41 @@
 package com.admuc.locationreminders.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Geocoder;
+import android.location.Location;
+import android.provider.SyncStateContract;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.admuc.locationreminders.R;
+import com.admuc.locationreminders.models.AutomaticReminder;
+import com.admuc.locationreminders.models.ManualReminder;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SelectLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+
+public class SelectLocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker locationMarker;
+    private com.admuc.locationreminders.models.Location selectedLocation = new com.admuc.locationreminders.models.Location(0,0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +45,32 @@ public class SelectLocationActivity extends FragmentActivity implements OnMapRea
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.circle_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    private String getAddressFromLatLng( LatLng latLng ) {
+        Geocoder geocoder = new Geocoder(this);
+
+        String address = "";
+        try {
+            address = geocoder
+                    .getFromLocation( latLng.latitude, latLng.longitude, 1 )
+                    .get( 0 ).getAddressLine( 0 );
+        } catch (IOException e ) {
+        }
+
+        return address;
     }
 
 
@@ -39,9 +87,67 @@ public class SelectLocationActivity extends FragmentActivity implements OnMapRea
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
+
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                if (locationMarker == null) {
+                    MarkerOptions options = new MarkerOptions().position(latLng).title(getAddressFromLatLng(latLng));
+                    locationMarker = mMap.addMarker(options);
+                } else {
+                    locationMarker.setPosition(latLng);
+
+                    selectedLocation.setLat(latLng.latitude);
+                    selectedLocation.setLon(latLng.longitude);
+                }
+            }
+        });
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+            @Override
+            public void onMyLocationChange(Location location) {
+
+                // move camera only first time by location detection
+                if (locationMarker == null) {
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+                    LatLng ll = new LatLng(lat, lng);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 20));
+                }
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.setLocation) {
+
+            Intent intent = new Intent(getApplicationContext(), ManageActivity.class);
+            intent.putExtra("REMINDER_LAT", selectedLocation.getLat());
+            intent.putExtra("REMINDER_LON", selectedLocation.getLon());
+            Log.d("LAT: ", String.valueOf(selectedLocation.getLat()));
+            Log.d("LON: ", String.valueOf(selectedLocation.getLon()));
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
