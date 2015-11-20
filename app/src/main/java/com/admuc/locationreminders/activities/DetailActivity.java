6dba4ade3803d465af7bc29eb3f73694794765e8
Matthes Lipke <matthes.lipke@gmail.com>
2015-11-20 13:40:46 +0100

    DetailView now draws its toolbar behind the system status bar.

--- app/src/main/java/com/admuc/locationreminders/activities/DetailActivity.java
+++ app/src/main/java/com/admuc/locationreminders/activities/DetailActivity.java
@@ -3,6 +3,7 @@ package com.admuc.locationreminders.activities;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +46,8 @@ import java.util.List;
 
 public class DetailActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback {
 
+    private Toolbar toolbar;
+
     private Reminder reminder;
 
     private long _id;
@@ -58,7 +63,7 @@ public class DetailActivity extends AppCompatActivity implements GoogleApiClient
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_18dp);
@@ -73,6 +78,11 @@ public class DetailActivity extends AppCompatActivity implements GoogleApiClient
         type = intent.getStringExtra("REMINDER_TYPE");
         _id = intent.getLongExtra("REMINDER_ID", 0);
 
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -194,11 +204,9 @@ public class DetailActivity extends AppCompatActivity implements GoogleApiClient
     private class MapListener implements OnMapReadyCallback {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-
+            toolbar.bringToFront();
         }
     }
-
-
 
     @Override
     public void onConnected(Bundle bundle) {
