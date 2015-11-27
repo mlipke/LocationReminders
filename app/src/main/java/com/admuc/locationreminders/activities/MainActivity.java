8ba8f854c4b1afdd4d75af0e46e626ad2d6ebdd8
Matthes Lipke <matthes.lipke@gmail.com>
2015-11-27 11:03:26 +0100

    New class for SimpleDividerItemDecoration.

--- app/src/main/java/com/admuc/locationreminders/activities/MainActivity.java
+++ app/src/main/java/com/admuc/locationreminders/activities/MainActivity.java
@@ -26,6 +26,7 @@ import com.admuc.locationreminders.models.ManualReminder;
 import com.admuc.locationreminders.models.Reminder;
 import com.admuc.locationreminders.utils.ReminderComparator;
 import com.admuc.locationreminders.utils.ReminderHelper;
+import com.admuc.locationreminders.utils.SimpleDividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -102,34 +103,6 @@ public class MainActivity extends AppCompatActivity {
 
         adapter.setReminders(reminders);
         adapter.notifyDataSetChanged();
-    }
-
-
-    public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
-        private Drawable mDivider;
-
-        public SimpleDividerItemDecoration(Context context) {
-            mDivider = context.getResources().getDrawable(R.drawable.line_divider);
-        }
-
-        @Override
-        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-            int left = parent.getPaddingLeft();
-            int right = parent.getWidth() - parent.getPaddingRight();
-
-            int childCount = parent.getChildCount();
-            for (int i = 0; i < childCount; i++) {
-                View child = parent.getChildAt(i);
-
-                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
-
-                int top = child.getBottom() + params.bottomMargin;
-                int bottom = top + mDivider.getIntrinsicHeight();
-
-                mDivider.setBounds(left, top, right, bottom);
-                mDivider.draw(c);
-            }
-        }
     }
 
     private class NavigationClickListener implements NavigationView.OnNavigationItemSelectedListener {
