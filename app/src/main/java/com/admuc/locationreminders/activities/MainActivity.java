077d96c0e2c9a90c6888177b745d2b782414c829
Matthes Lipke <matthes.lipke@gmail.com>
2016-01-12 11:53:36 +0100

    Added manual service control

--- app/src/main/java/com/admuc/locationreminders/activities/MainActivity.java
+++ app/src/main/java/com/admuc/locationreminders/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.admuc.locationreminders.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.admuc.locationreminders.R;
@@ -27,12 +29,16 @@ public class MainActivity extends AppCompatActivity {
     private ActiveRemindersFragment activeRemindersFragment;
     private CompletedRemindersFragment completedRemindersFragment;
 
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,8 +49,22 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(viewPager);
 
-        Intent locationServiceIntent = new Intent(this, LocationService.class);
-        startService(locationServiceIntent);
+        if (preferences.getBoolean("pref_first_start", true)) {
+            startLocationService();
+            preferences.edit()
+                    .putBoolean("pref_first_start", false)
+                    .putBoolean("pref_service_running", true)
+                    .apply();
+        } else {
+            if (preferences.getBoolean("pref_manual_control", false)) {
+                if (!preferences.getBoolean("pref_service_running", false)) {
+                    startLocationService();
+                }
+            } else {
+                startLocationService();
+            }
+        }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -64,11 +84,24 @@ public class MainActivity extends AppCompatActivity {
             activeRemindersFragment.notifyDataSetChanged();
         if (completedRemindersFragment != null)
             completedRemindersFragment.notifyDataSetChanged();
+
+        invalidateOptionsMenu();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        if (preferences.getBoolean("pref_manual_control", false)) {
+            menu.getItem(0).setVisible(true);
+        }
+
+        if (preferences.getBoolean("pref_service_running", false)) {
+            menu.getItem(0).setIcon(R.drawable.ic_gps_fixed_24dp);
+        } else {
+            menu.getItem(0).setIcon(R.drawable.ic_gps_not_fixed_24dp);
+        }
+
         return true;
     }
 
@@ -76,12 +109,37 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
+        switch (id) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_manual_control:
+                toggleLocationService(item);
+                break;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startLocationService() {
+        Intent locationServiceIntent = new Intent(this, LocationService.class);
+        startService(locationServiceIntent);
+    }
+
+    private void toggleLocationService(MenuItem item) {
+        Intent intent = new Intent(this, LocationService.class);
+        if (preferences.getBoolean("pref_service_running", false)) {
+            stopService(intent);
+            item.setIcon(R.drawable.ic_gps_not_fixed_24dp);
+            preferences.edit().putBoolean("pref_service_running", false).apply();
+        } else {
+            startService(intent);
+            item.setIcon(R.drawable.ic_gps_fixed_24dp);
+            preferences.edit().putBoolean("pref_service_running", true).apply();
+        }
     }
 
     private void setupViewPager(ViewPager viewPager) {
